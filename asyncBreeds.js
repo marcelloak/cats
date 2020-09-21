cfff99dbad79f2e60a643eea06fb083c2d0aaa8d
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-09-21 17:31:19 -0600

    Added Async and Sync

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, done) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) done(data);
+  });
+};
+
+const printOutCatBreed = breed => console.log(breed);
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
